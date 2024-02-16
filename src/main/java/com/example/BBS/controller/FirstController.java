d6585c941a9c9a9fa9fe682542e33157c3619088
JoonYong <joonyongpark99@gmail.com>
2024-02-16 22:02:28 +0900

    Adding a Simple Greeting

---
+++ src/main/java/com/example/BBS/controller/FirstController.java
@@ -0,0 +1,22 @@
+package com.example.BBS.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public  String niceToMeetYou(Model model) {
+        model.addAttribute("username", "준용");
+        return "hello";   // templates/greetings.mustache를 브라우저로 렌더링합니다.
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "준용");
+        return "goodbye";
+    }
+
+}
