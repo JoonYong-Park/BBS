a478cba9196919081f990b8fd670c67b7d5ef9b8
JoonYong <joonyongpark99@gmail.com>
2024-03-02 18:46:36 +0900

    API Controller

---
+++ src/main/java/com/example/BBS/api/ArticleApiController.java
@@ -0,0 +1,88 @@
+package com.example.BBS.api;
+
+import com.example.BBS.dto.ArticleForm;
+import com.example.BBS.entity.Article;
+import com.example.BBS.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController // RestAPI용 컨트롤러! JSON을 반환!
+public class ArticleApiController {
+
+    // 레파지토리를 주입받음
+    @Autowired // DI
+    private ArticleRepository articleRepository;
+
+    /// GET ///
+    // 전체
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+    // 단일
+    @GetMapping("/api/articles/{id}")
+    public Article index(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    /// POST ///
+    @PostMapping("/api/articles")
+    public Article create(@RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        return articleRepository.save(article);
+    }
+
+    /// PATCH ///
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+
+        // 1. 수정용 엔티티 생성
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        // 2. 대상 엔티티를 조회
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 3. 잘못된 요청 처리(대상이 없거나. id가 다른경우)
+        if (target == null || id != article.getId()) {
+            // 400 잘못된 요청
+            log.info("id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        // 4. 업데이트 및 정상 응답(200)
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    /// DELETE ///
+    // 단일
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+
+        // 1. 대상 엔티티 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 2. 잘못된 요청 처리
+        if (target == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        // 3. 삭제
+        articleRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+    // 전체
+    @DeleteMapping("/api/articles")
+    public ResponseEntity<Article> deleteAll() {
+        articleRepository.deleteAll();
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+}
--- src/main/java/com/example/BBS/entity/Article.java
+++ src/main/java/com/example/BBS/entity/Article.java
@@ -16,10 +16,21 @@ public class Article {
     @Id     //대표값 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 1, 2, 3, ... 자동 생성 어노테이션/ (DB가 id를 자동생성)
     private Long id;
+
     @Column  // 컬럼으로 지정
     private String title;
+
     @Column
     private String content;
+
+    public void patch(Article article) {
+        if (article.getTitle() != null) {
+            this.title = article.getTitle();
+        }
+        if (article.getContent() != null) {
+            this.content = article.getContent();
+        }
+    }
 
 /*    public Long getId() {
         return id;
